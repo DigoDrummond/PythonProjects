025cdfae3c4318ec0deb7078b517f303918c7fd6
Rodrigo Drummond <rodrigodm2810@gmail.com>
2024-03-04 21:15:52 -0300

    Automated file backup

---
+++ AutomatedFileBackup/main.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "C:\\Users\\rodri\\OneDrive\\Área de Trabalho\\Finances"
+destination_dir = "C:\\Users\\rodri\\OneDrive\\Área de Trabalho\\teste"
+
+def copy_folder_to_directory(source,dest):
+    today = datetime.date.today()#gives the curretn date
+    dest_dir = os.path.join(dest,str(today))
+    
+    try:
+        shutil.copytree(source,dest_dir)
+        print(f"Folder copied to {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+schedule.every().day.at("21:16").do(lambda: copy_folder_to_directory(source_dir,destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
