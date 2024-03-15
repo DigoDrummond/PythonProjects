7ebe5b25a84ded40a7b31a40398307b0c1c7b7bc
Rodrigo Drummond <rodrigodm2810@gmail.com>
2024-03-14 21:36:59 -0300

    Wpm typing test finished

--- WPMTypingTest/main.py
+++ WPMTypingTest/main.py
@@ -1,5 +1,7 @@
 import curses
 from curses import wrapper
+import time
+import random
 
 def start_screen(stdscr):
     stdscr.clear()#clear terminal
@@ -9,27 +11,48 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 def display_text(stdscr,target_text,current_text,wpm=0):
-    stdscr.addstr(target_text) 
+    stdscr.addstr(target_text)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
          
     for i, char in enumerate(current_text):
-        stdscr.addstr(0,i,char,curses.color_pair(1))
+        correct_char = target_text[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+            
+        stdscr.addstr(0, i, char, color)
     
-
+def load_text():
+    with open("C:\\Users\\rodri\\OneDrive\\Área de Trabalho\\PythonProjects\\WPMTypingTest\\text.txt","r",encoding="utf-8") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
 
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = load_text()
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)#do not delay waiting for the user to hit a key
    
     
     while(True):
+        time_elapsed = max(time.time() - start_time,1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+        
         stdscr.clear()#clear terminal
-        
-        display_text(stdscr,target_text,current_text)
-        
+        display_text(stdscr,target_text,current_text,wpm)
         stdscr.refresh()
         
-        key = stdscr.getkey()
-       
+        #combine caracters from the list in a string
+        if "".join(current_text) == target_text:
+          stdscr.nodelay(False)
+          break
+        
+        try:
+            key = stdscr.getkey()
+        except:
+            continue#brings back to the top of the while loop
+        
         #escape Esc
         if ord(key) == 27:
             break
@@ -37,13 +60,8 @@ def wpm_test(stdscr):
         if key in ("KEY_BACKSPACE",'\b',"\x7f"):
             if len(current_text) > 0:
                 current_text.pop()#remove the last element from the list
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
-        
-       
-        
-       
-    
         
         
 
@@ -54,7 +72,12 @@ def main(stdscr):
     curses.init_pair(3,curses.COLOR_WHITE,curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+        if ord(key) == 27:
+            break
     
 wrapper(main)
 
